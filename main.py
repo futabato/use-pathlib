e0b236c0d9c5fefcdca238a34ae28372673156d3
futabato <01futabato10@gmail.com>
2025-04-23 14:51:15 +0900

    style(main.py): remove unnecessary f-string formatting for print statements to enhance code readability

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ print(os.listdir('/'), end='\n\n')
 
 # ファイルを作成して、誤って削除してしまうシナリオ
 # /workspace/log を削除しようとするが、誤ってルートの /log が削除される
-print(f"削除したいパス: /workspace/log", end='\n\n')
+print("削除したいパス: /workspace/log", end='\n\n')
 delete_path = os.path.join('/workspace', '/log')  # 引数が絶対パスであるとその前の引数は無視される
-print(f"削除されるパス: /log", end='\n\n')
+print("削除されるパス: /log", end='\n\n')
 
 os.rmdir(delete_path)  # /logが削除される（/workspace/logではなくルートの /log）
 
