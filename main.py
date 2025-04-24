d9fb3e871d972e7c7cc3cf81c1b2f77a5f640c51
futabato <01futabato10@gmail.com>
2025-04-24 14:41:21 +0900

    docs(main.py): update comments to clarify the directory deletion scenario and correct the output path for better understanding

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ os.makedirs('/log', exist_ok=True)
 print("ルートディレクトリの内容：")
 print(os.listdir('/'), end='\n\n')
 
-# ファイルを作成して、誤って削除してしまうシナリオ
+# os.path.join の仕様により誤ってディレクトリを削除してしまうシナリオ
 # /workspace/log を削除しようとするが、誤ってルートの /log が削除される
 print("削除したいパス: /workspace/log", end='\n\n')
 delete_path = os.path.join('/workspace', '/log')  # 引数が絶対パスであるとその前の引数は無視される
-print("削除されるパス: /log", end='\n\n')
+print(f"削除されるパス: {delete_path}", end='\n\n')
 
 os.rmdir(delete_path)  # /logが削除される（/workspace/logではなくルートの /log）
 
